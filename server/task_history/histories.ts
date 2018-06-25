f7871fa10ead139821ec855f2f211e48644c2b92
unknown <B7306@oic.osaka.jp>
2018-06-25 11:34:02 +0900

    履歴の表示形式を修正した

--- server/task_history/histories.ts
+++ server/task_history/histories.ts
@@ -32,30 +32,19 @@ export class UpdateHistory extends BaseHistory {
         this.after_task = after_task;
     }
     show() {
-        let before_task_name = "";
-        let after_task_name = "";
-        let before_task_head = "";
-        let after_task_head = "";
-        let before_task_description = "";
-        let after_task_description = "";
-        let before_task_state = "";
-        let after_task_state = "";
+        let str = "";
         if (this.before_task.Name != this.after_task.Name) {
-            before_task_name = `タスク名:${this.before_task.Name}`;
-            after_task_name = `タスク名:${this.after_task.Name}`;
+            str += `{タスク名:${this.before_task.Name}→${this.after_task.Name}}`;
         }
         if (this.before_task.Head != this.after_task.Head) {
-            before_task_head = `見出し:${this.before_task.Head}`;
-            after_task_head = `見出し:${this.after_task.Head}`;
+            str += `{見出し:${this.before_task.Head}→${this.after_task.Head}}`;
         }
         if (this.before_task.Description != this.after_task.Description) {
-            before_task_description = `説明:${this.before_task.Description}`;
-            after_task_description = `説明:${this.after_task.Description}`;
+            str += `{説明:${this.before_task.Description}→${this.after_task.Description}}`;
         }
         if (this.before_task.State != this.after_task.State) {
-            before_task_state = `状況:${this.before_task.State}`;
-            after_task_state = `状況:${this.after_task.State}`;
+            str += `{状況:${this.before_task.State}→${this.after_task.State}}`;
         }
-        return `{${before_task_name},${before_task_head},${before_task_description},${before_task_state}}を{${after_task_name},${after_task_head},${after_task_description},${after_task_state}}に更新`;
+        return `${str}に更新`;
     }
 }
